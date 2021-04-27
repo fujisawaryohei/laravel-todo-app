50d5f7ad499a08be6c90aaf078866bbb0f4a52aa
fujisawaryohei <ryoheifujjiawa@gmail.com>
2021-04-28 02:03:10 +0900

    Postモデルにgetter, setterの追加

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillabel = [
+      'title', 'body', 'is_public', 'published_at'
+    ];
+
+    protected $guarded = [];
+
+    protected $casts = [
+      'is_public' => 'bool',
+      'published_at' => 'datetime'
+    ];
 }
